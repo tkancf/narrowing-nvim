b4ea25e4b5bf1b3b8647437ca4167a79ba37b145
tkancf <18424095+tkancf@users.noreply.github.com>
2025-06-22 17:37:00 +0900

    feat: change command structure to use subcommands
    - Replace separate commands (Narrow, NarrowWrite, NarrowQuit) with
      single Narrowing command that takes subcommands
    - Add command completion for subcommands
    - Update documentation to reflect new command structure
    
    Closes #1

--- plugin/narrowing.lua
+++ plugin/narrowing.lua
@@ -10,17 +10,28 @@ vim.g.loaded_narrowing = 1
 
 local narrowing = require("narrowing")
 
-vim.api.nvim_create_user_command("Narrow", function()
-  narrowing.narrow()
-end, { range = true })
-
-vim.api.nvim_create_user_command("NarrowWrite", function()
-  narrowing.write()
-end, {})
-
-vim.api.nvim_create_user_command("NarrowQuit", function()
-  narrowing.quit()
-end, {})
+vim.api.nvim_create_user_command("Narrowing", function(opts)
+  local subcommand = opts.fargs[1]
+  
+  if not subcommand or subcommand == "narrow" then
+    narrowing.narrow()
+  elseif subcommand == "write" then
+    narrowing.write()
+  elseif subcommand == "quit" then
+    narrowing.quit()
+  else
+    vim.api.nvim_err_writeln("Unknown subcommand: " .. subcommand)
+    vim.api.nvim_echo({
+      {"Usage: :Narrowing [narrow|write|quit]", "Normal"}
+    }, false, {})
+  end
+end, { 
+  range = true,
+  nargs = "?",
+  complete = function(_, _, _)
+    return { "narrow", "write", "quit" }
+  end,
+})
 
 vim.keymap.set("v", "<Plug>(narrowing-narrow)", function()
   narrowing.narrow()
